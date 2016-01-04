1503c7a379306e98dd41675806f1eb4b32af122a
ghaiklor <ghaiklor@gmail.com>
2016-01-04 18:06:17 +0200

    Finish day-22 part-1

--- day-22/part-1/solution.js
+++ day-22/part-1/solution.js
@@ -1,61 +1,146 @@
-"use strict";
+'use strict';
 
-const SPELLS = new Map([
-  ['Magic Missile', {cost: 53, damage: 4, heals: 0, effect: false}],
-  ['Drain', {cost: 73, damage: 2, heals: 2, effect: false}],
-  ['Shield', {cost: 113, damage: 0, heals: 0, effect: 'Shield'}],
-  ['Poison', {cost: 173, damage: 0, heals: 0, effect: 'Poison'}],
-  ['Recharge', {cost: 229, damage: 0, heals: 0, effect: 'Recharge'}]
-]);
+class Player {
+  constructor(initial, isWizard) {
+    this.history = [];
+    this.initial = initial;
+    this.isWizard = !!isWizard;
 
-const EFFECTS = new Map([
-  ['Shield', {duration: 6, armor: 7, damage: 0, mana: 0}],
-  ['Poison', {duration: 6, armor: 0, damage: 3, mana: 0}],
-  ['Recharge', {duration: 5, armor: 0, damage: 0, mana: 101}]
-]);
+    if (this.isWizard) {
+      this.spells = [{
+        cost: 53,
+        effect: (m, o) => o.damage(4)
+      }, {
+        cost: 73,
+        effect: (m, o) => {
+          o.damage(2);
+          m.hp += 2;
+        }
+      }, {
+        cost: 113,
+        start: (m, o) => m.armor += 7,
+        effect: (m, o) => {
+        },
+        end: (m, o) => m.armor -= 7,
+        duration: 6
+      }, {
+        cost: 173,
+        effect: (m, o) => o.damage(3),
+        duration: 6
+      }, {
+        cost: 229,
+        effect: (m, o) => m.mana += 101,
+        duration: 5
+      }];
+    }
 
-const BOSS = new Map([
-  ['hp', 55],
-  ['damage', 8],
-  ['armor', 0],
-  ['mana', 0],
-  ['effects', new Map()]
-]);
-
-const PLAYER = new Map([
-  ['hp', 50],
-  ['damage', 0],
-  ['armor', 0],
-  ['mana', 500],
-  ['effects', new Map()]
-]);
-
-const applyEffect = (player, effect) => {
-  const effects = player.get('effects');
-  if (effects.has(effect)) return;
-
-  effects.set(effect, EFFECTS.get(effect));
-};
-
-const attack = (attacker, defender, spell) => {
-  if (attacker.has('mana')) {
-    attacker.set('mana', attacker.get('mana') - spell.cost);
-    attacker.set('hp', attacker.get('hp') + spell.heals);
-    defender.set('hp', defender.get('hp') - spell.damage);
-    if (spell.effect) applyEffect(attacker, spell.effect);
-  } else {
-    defender.set('hp', defender.get('hp') - Math.max(1, attacker.get('damage') - defender.get('armor')));
+    this.start();
   }
 
-  const effects = PLAYER.get('effects');
-  effects.forEach(effect => {
-    if (effect.duration > 0) {
-      PLAYER.set('armor', effect.armor);
-      PLAYER.set('damage', effect.damage);
-      PLAYER.set('mana', PLAYER.get('mana') + effect.mana);
-      effect.set('duration', effect.get('duration') - 1);
+  attack(opponent, spellIdx) {
+    if (!this.isWizard) {
+      opponent.damage(this.damageAmt);
+    } else {
+      let spell = this.spells[spellIdx];
+      this.history.push(spellIdx);
+      this.spent += spell.cost;
+      this.mana -= spell.cost;
 
-      if (effect.duration === 0) effects.delete(effect);
+      if (spell.duration) {
+        let newSpell = {idx: spellIdx, effect: spell.effect, duration: spell.duration};
+        if (spell.start) spell.start(this, opponent);
+        if (spell.end) newSpell.end = spell.end;
+        this.activeSpells.push(newSpell);
+      } else {
+        spell.effect(this, opponent);
+      }
     }
-  });
-};
+  }
+
+  damage(n) {
+    this.hp -= Math.max(1, n - this.armor);
+  }
+
+  duplicate() {
+    let newPlayer = new Player(this.initial, this.isWizard);
+
+    newPlayer.hp = this.hp;
+    newPlayer.spent = this.spent;
+    newPlayer.armor = this.armor;
+    newPlayer.turn = this.turn;
+
+    for (let i = 0; i < this.activeSpells.length; i++) newPlayer.activeSpells.push(Object.assign({}, this.activeSpells[i]));
+    for (let i = 0; i < this.history.length; i++) newPlayer.history.push(this.history[i]);
+
+    if (this.isWizard) {
+      newPlayer.mana = this.mana;
+    } else {
+      newPlayer.damageAmt = this.damageAmt;
+    }
+
+    return newPlayer;
+  }
+
+  takeTurn(opponent) {
+    this.turn++;
+
+    for (let i = 0; i < this.activeSpells.length; i++) {
+      let spell = this.activeSpells[i];
+
+      if (spell.duration > 0) {
+        spell.effect(this, opponent);
+        spell.duration--;
+
+        if (spell.duration === 0 && spell.end) spell.end(this, opponent);
+      }
+    }
+  }
+
+  start() {
+    this.hp = this.initial.hp;
+    this.spent = 0;
+    this.armor = 0;
+    this.turn = 0;
+    this.activeSpells = [];
+    if (this.isWizard) {
+      this.mana = this.initial.mana;
+    } else {
+      this.damageAmt = this.initial.damageAmt;
+    }
+  }
+}
+
+let me = new Player({hp: 50, mana: 500}, true);
+let boss = new Player({hp: 55, damageAmt: 8});
+let cheapestSpent = Infinity;
+
+function playAllGames(me, boss) {
+  for (let i = 0; i < me.spells.length; i++) {
+    let spellMatch = false;
+
+    for (let j = 0; j < me.activeSpells.length; j++) {
+      if (me.activeSpells[j].duration > 1 && i === me.activeSpells[j].idx) spellMatch = true;
+    }
+
+    if (spellMatch) continue;
+    if (me.spells[i].cost > me.mana) continue;
+
+    let newMe = me.duplicate();
+    let newBoss = boss.duplicate();
+
+    newMe.takeTurn(newBoss);
+    newBoss.takeTurn(newMe);
+    newMe.attack(newBoss, i);
+
+    newMe.takeTurn(newBoss);
+    newBoss.takeTurn(newMe);
+    newBoss.attack(newMe);
+
+    if (newBoss.hp <= 0) cheapestSpent = Math.min(cheapestSpent, newMe.spent);
+    if (newMe.hp > 0 && newBoss.hp > 0 && newMe.spent < cheapestSpent) playAllGames(newMe, newBoss);
+  }
+}
+
+playAllGames(me, boss);
+
+console.log(cheapestSpent);
