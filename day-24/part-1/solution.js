293c9eaacfa09174655d06ef267defe2cfa06d1d
ghaiklor <ghaiklor@gmail.com>
2016-01-04 13:57:18 +0200

    Finish day-24 part-1

---
+++ day-24/part-1/solution.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const Combinatorics = require('./combinatorics');
+const fs = require('fs');
+const INPUT = fs.readFileSync('./input.txt', 'utf-8').split('\n').map(Number);
+const TOTAL_SUM = INPUT.reduce((total, x) => total + x, 0);
+const GROUP_WEIGHT = TOTAL_SUM / 3;
+const VALID_PACKAGES = [];
+
+for (var i = 1; VALID_PACKAGES.length === 0; i++) {
+  var combination = Combinatorics.combination(INPUT, ++i);
+  var cmb;
+
+  while (cmb = combination.next()) {
+    if (cmb.reduce((acc, x) => acc + x) === GROUP_WEIGHT) VALID_PACKAGES.push(cmb);
+  }
+}
+
+const result = VALID_PACKAGES.map(pkg => pkg.reduce((acc, x) => acc * x)).sort((a, b) => a - b)[0];
+
+console.log(result);
